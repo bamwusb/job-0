e4623341860847b35f10127ec5dd9c7d5d7a6113
Demo github account <bam@wusb.fm>
2015-08-10 19:10:11 -0400

    Update j0.java

--- j0.java
+++ j0.java
@@ -1,5 +1,5 @@
 //// This is your starter code.
-//// Put your username here.
+//// *** bamwusb ***
 
 size( 640, 480 );
 background( 50, 100, 200 );
@@ -9,10 +9,6 @@ fill(255,0,0);
 rectMode(CENTER);
 rect( 100,100, 30,50 );
 
+// bamwusb made some changes here.
 
-//  Modify the above code as follows:
-//    * change the color of the body.
-//    * add a head 
-//    * make it follow the mouse.
 
-//  When you are done, commit your code and send me a "pull-request"
